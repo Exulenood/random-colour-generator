14b28a1f0dcd502b24b90dfafa6e05b7af2d414b
Exulenood <119940832+Exulenood@users.noreply.github.com>
2023-01-10 20:27:13 +0100

    Installed chalk Tested OK

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// console.log(rcolor()); // tested 10.1.2023 20:12 after installing rcolor: OK! Output: #79a8f2
+import chalk from 'chalk';
 import rcolor from 'rcolor';
 
-// console.log(rcolor()); // tested 10.1.2023 20:12 after installing rcolor: OK! Output: #79a8f2
+// console.log(chalk.hex('##79a8f2')('this is a color test')); // tested 10.1.2023 20:25 after installing chalk: OK! Output: 'this is a color test' in #79a8f2
